bef62afa5222810995e12e8aaafc79d9a67b7c04
Mariusz Brozda <mariuszbrozda.dublin@gmail.com>
2026-06-19 00:24:59 +0100

    Update eks-cluster module main.tf

--- infra/terraform/modules/eks-cluster/main.tf
+++ infra/terraform/modules/eks-cluster/main.tf
@@ -1,22 +1,22 @@
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 19.0"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
- 
-  vpc_id                           = var.vpc_id
-  subnet_ids                       = var.subnet_ids
-  cluster_endpoint_private_access  = var.cluster_endpoint_private_access
-  cluster_endpoint_public_access   = var.cluster_endpoint_public_access
+
+  vpc_id                               = var.vpc_id
+  subnet_ids                           = var.subnet_ids
+  cluster_endpoint_private_access      = var.cluster_endpoint_private_access
+  cluster_endpoint_public_access       = var.cluster_endpoint_public_access
   cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
 
   eks_managed_node_groups = {
     default = {
-      desired_size = var.node_desired_capacity
-      max_size     = var.node_max_capacity
-      min_size     = var.node_min_capacity
-      instance_types  = var.node_instance_types
+      desired_size   = var.node_desired_capacity
+      max_size       = var.node_max_capacity
+      min_size       = var.node_min_capacity
+      instance_types = var.node_instance_types
     }
   }
 
